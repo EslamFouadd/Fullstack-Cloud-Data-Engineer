a3f499b6ed5bd123ecd0402e224a8c9d1e06b2dd
Sam Fouad <77150715+EslamFouadd@users.noreply.github.com>
2023-08-08 05:23:46 +0300

    Create query_1.sql

---
+++ lab03/query_1.sql
@@ -0,0 +1,13 @@
+/* Create tables from other tables with DDL */
+
+#standardSQL
+CREATE TABLE
+  nyctaxi.january_trips AS
+SELECT
+  *
+FROM
+  nyctaxi.2018trips
+WHERE
+  EXTRACT(Month
+  FROM
+    pickup_datetime)=1;
